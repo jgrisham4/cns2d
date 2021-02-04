b36a8784ee51a798bc06ff9ed13db757db83da75
jgrisham4 <jgrisham4@gmail.com>
2021-02-04 07:06:36 -0800

    Adding local change to run_tests.py.

--- src/run_tests.py
+++ src/run_tests.py
@@ -6,7 +6,8 @@ from subprocess import call
 lapack95_path ="/opt/intel/compilers_and_libraries_2016.3.210/linux/mkl/include/intel64/lp64"
 
 #funit_args = "FCFLAGS='-I/share/CGNS-intel/build/include -I{}' LDFLAGS='-L/share/CGNS-intel/build/lib -mkl'".format(lapack95_path)
-funit_args = "FCFLAGS='-I/share/CGNS-intel/build/include' LDFLAGS='-L/share/CGNS-intel/build/lib -mkl'"
+#funit_args = "FCFLAGS='-I/share/CGNS-intel/build/include' LDFLAGS='-L/share/CGNS-intel/build/lib -mkl'"
+funit_args = "FCFLAGS='-I/share/CGNS/build/include' LDFLAGS='-L/share/CGNS/build/lib'"
 #funit_args = "FCFLAGS='-I/share/CGNS-intel/build/include' LDFLAGS='/share/CGNS-intel/build/lib/libcgns.a'"
 #print("\ncalling funit with {}\n".format(funit_args))
 call("source ~/.bashrc; source ~/.funit; {} funit".format(funit_args), shell=True)
